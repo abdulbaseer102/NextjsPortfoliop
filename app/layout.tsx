af8ae4c8ed052e20a3edcfa2b1c63b759a7827ff
abdulbaseer102 <147842675+abdulbaseer102@users.noreply.github.com>
2024-09-10 06:44:27 +0500

    Commit message

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { ThemeProvider } from "./componets/theme-provider";
-
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,14 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-            <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
+        {children}
       </body>
     </html>
   );
